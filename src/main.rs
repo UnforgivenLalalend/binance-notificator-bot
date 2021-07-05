d28a8f2f9e04a96b0b4cae7da9f8bdb55e6bd951
Lalalend <acc4anim@gmail.com>
2021-07-05 15:17:21 +0300

    new libs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use binance::api::*;
+use binance::market::*;
+
+fn get_crypto_price(crypto_name: &str) -> Result<f64, anyhow::Error> {
+    let market: Market = Binance::new(None, None);
+
+    match market.get_price(crypto_name) {
+        Ok(symbol_price) => Ok(symbol_price.price),
+        Err(err) => Err(anyhow::anyhow!(
+            "Error, while parsing crypto price: {:?}",
+            err
+        )),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", get_crypto_price("NEARUDT"));
 }
